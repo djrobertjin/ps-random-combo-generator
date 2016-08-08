8bf387d4aea2f7ecbcc07412b3a9a4228d0cb54e
Robert Jin <robert.jin@vgbio.local>
2016-08-08 06:50:09 -0500

    Cleaned code

--- mysite/randomGen/views.py
+++ mysite/randomGen/views.py
@@ -1,32 +1,10 @@
-from django.shortcuts import render, get_object_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
-from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import generic
-from django.utils import timezone
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.template import RequestContext
 import json
-
 from .models import Trick
 from .forms import randomGenForm
 import random
-
-
-# class IndexView(generic.FormView):
-#     template_name = 'randomGen/index.html'
-#     #context_object_name = 'latest_tricks_list'
-#     form_class = randomGenForm
-#     success_url = ''
-
-#     def form_valid(self, form):
-#       return 1
-
-    # def get_queryset(self):
-    #   """
-    #   Return 
-    #   """
-
-    #   return [1,2,3]#Trick.objects.all()
 
 def home(request):
   form_class = randomGenForm
@@ -34,11 +12,8 @@ def home(request):
 
   return render(request, 'randomGen/index.html', context)
 
-#@ensure_csrf_cookie
-
 def generate(request):
-  
-  #context = {'all_trick_list': all_trick_list, 'form': form_class}
+
   if request.method == 'POST':
     excl_Sonic = str_to_bool(request.POST.get('excl_Sonic'))
     excl_Palm = str_to_bool(request.POST.get('excl_Palm'))
@@ -52,7 +27,6 @@ def generate(request):
       all_trick_list = all_trick_list.exclude(family_choices = 'sonic')
     if excl_family_choices:
       all_trick_list = all_trick_list.exclude(family_choices = excl_family_choices)
-    #combo = desired_length
     combo = [random.choice([trick.name + ' ' + trick.direction_choices for trick in all_trick_list]) for x in range(int(desired_length))]
     response_data['result'] = ' -> '.join(combo)
     response_data['name'] = 'Jack'
@@ -60,20 +34,7 @@ def generate(request):
     return JsonResponse(response_data)
   else:
     return JsonResponse({"ERROR": "error"})
-    # return HttpResponse(
-    #   json.dumps(response_data),
-    #   content_type="application/json"
-    #   )
-  # # else:
-  #   return HttpResponse(
-  #     json.dumps({"nothing to see": "this isn't happening"}),
-  #     content_type="application/json"
-  #     )
 
-  # context = {'all_trick_list': all_trick_list, 'form': form_class}
-  # return render(request, 'randomGen/index.html', context, RequestContext(request))
-
-  # return render(request, 'randomGen/index.html', context)
 
 def str_to_bool(s):
     if s == 'True' or s == 'true':
@@ -83,15 +44,3 @@ def str_to_bool(s):
     else:
          raise ValueError
   
-
-
-  # def get_length(request):
-  #     if request.method == 'POST':
-  #       form = randomGenForm(request.POST)
-  #       if form.is_valid():
-  #         return HttpResponseRedirect(' / ')
-
-  #     else:
-  #       form = randomGenForm()
-
-  #     return render(request, 'index.html', {'form': form})
